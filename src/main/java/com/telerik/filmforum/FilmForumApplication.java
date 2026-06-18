988d1ba9fff3ec13a0e607fa0eaf278612e9c77d
gkachura <135459011+gkachura@users.noreply.github.com>
2026-06-18 16:07:03 +0300

    initial commit

---
+++ src/main/java/com/telerik/filmforum/FilmForumApplication.java
@@ -0,0 +1,13 @@
+package com.telerik.filmforum;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FilmForumApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FilmForumApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/telerik/filmforum/FilmForumApplicationTests.java
@@ -0,0 +1,13 @@
+package com.telerik.filmforum;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FilmForumApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
