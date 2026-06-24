ef642976fb192e738c3d4b3167d28c071c3a57c0
Aleksandar Plamenov <you@example.com>
2026-06-24 21:14:53 +0300

    fixing Date format fields

--- src/main/java/com/telerik/filmforum/models/Post.java
+++ src/main/java/com/telerik/filmforum/models/Post.java
@@ -1,12 +1,12 @@
 package com.telerik.filmforum.models;
 
-import org.springframework.format.annotation.DateTimeFormat;
+import java.time.LocalDateTime;
 
 public class Post {
     private int id;
     private String title;
     private String content;
     private int authorId;
-    private DateTimeFormat createdAt;
-    private DateTimeFormat updatedAt;
+    private LocalDateTime createdAt;
+    private LocalDateTime updatedAt;
 }
